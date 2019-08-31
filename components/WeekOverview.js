9c07c020f5326591f5b22d3a787cd4bfe1c74e57
Dusan Baran <work.dusanbaran@gmail.com>
2019-08-31 17:45:50 +0200

    Add WeekOverview dummy component

---
+++ components/WeekOverview.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Text} from 'react-native'
+
+const WeekOverview = () => {
+  return (
+    <Text style={{flex: 1, backgroundColor: '#eeeeee'}}>WeekOverview Component</Text>
+  )
+}
+
+export default WeekOverview
