b0dd42acfbf752c431c14c70cf1d0f57ff4b89e0
Dusan Baran <work.dusanbaran@gmail.com>
2019-08-31 16:28:38 +0200

    Add state handling to MainButton

--- components/MainButton.js
+++ components/MainButton.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {TouchableOpacity, Text} from 'react-native'
 
 import {GaugeProgress} from 'react-native-simple-gauge'
 
 import styles, {buttonSize, gaugeWidth} from './MainButton.style'
 
+const percentage = (f, s) => f / s * 100 || 0
+
 const MainButton = () => {
+  const all = 1
+  const [visited, setVisited] = useState(0)
+
   return (
      <GaugeProgress
        size={buttonSize + gaugeWidth * 2}
        width={gaugeWidth}
-       fill={69}
+       fill={percentage(visited, all)}
        cropDegree={180}
        tintColor="#4682b4"
        backgroundColor="#b0c4de">
