474f626f644272542ac6a62bcd61d3726f653d5c
Dusan Baran <work.dusanbaran@gmail.com>
2019-08-31 17:46:16 +0200

    Rework MainScreen to divide screen for components

--- components/MainScreen.js
+++ components/MainScreen.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import {Text} from 'react-native'
+import {View} from 'react-native'
 
+import WeekOverview from './WeekOverview'
 import MainButton from './MainButton'
+import styles from './MainScreen.style'
 
 const MainScreen = () => {
   return (
-    <MainButton />
+    <View style={styles.mainView}>
+      <View style={styles.weekOverview}>
+        <WeekOverview />
+      </View>
+      <View style={styles.mainButton}>
+        <MainButton />
+      </View>
+    </View>
   )
 }
 
---
+++ components/MainScreen.style.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native'
+
+export default StyleSheet.create({
+  mainView: {
+    alignItems: 'stretch',
+    flex: 1,
+  },
+  weekOverview: {
+    flex: 3,
+  },
+  mainButton: {
+    flex: 2,
+  }
+})
