344b412f196bbdf6500f030b68260aac3ba71edb
Michal Kimle <kimle.michal@gmail.com>
2019-09-02 21:20:54 +0200

    Add NewEventScreen and routing

---
+++ src/components/NewEventScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Text} from 'react-native'
+
+const NewEventScreen = () => {
+  return (
+    <Text>NewEvent Screen</Text>
+  )
+}
+
+export default NewEventScreen
