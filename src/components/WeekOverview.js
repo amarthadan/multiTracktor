fbc1ac8b6e0d63eea23e260a8884c393b916ea42
Dušan Baran <work.dusanbaran@gmail.com>
2019-09-09 16:11:52 +0200

    Add WeekOverview component

--- src/components/WeekOverview.js
+++ src/components/WeekOverview.js
@@ -1,9 +1,25 @@
 import React from 'react'
-import {Text} from 'react-native'
+import {Text, View} from 'react-native'
+
+import _ from 'lodash'
+
+const DAY = 3600000*24
+
+const week = () => _.range(7).map(d => new Date(Date.now() - DAY * d))
 
 const WeekOverview = () => {
+  const overview = week().map(d =>
+    <View key={d.toString()} style={{flex: 1}}>
+      <Text>
+        {d.toString()}
+      </Text>
+    </View>
+  )
+
   return (
-    <Text style={{flex: 1, backgroundColor: '#eeeeee'}}>WeekOverview Component</Text>
+    <View style={{flex: 1}}>
+      {overview}
+    </View>
   )
 }
 
