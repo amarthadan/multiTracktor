3d1759d16fe916b104221c97990bb53c3ad11a24
Michal Kimle <kimle.michal@gmail.com>
2019-09-24 22:19:51 +0200

    Move position retrieval to `NewEventScreen`
    This way position retrieval won't be called for each button.

--- src/components/NewEventScreen.js
+++ src/components/NewEventScreen.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {View} from 'react-native'
+import {useSelector} from 'react-redux'
+
+import {selectedCoordinatesSelector, currentPositionSelector} from '../redux/selectors'
 
 import NewEventButton from './NewEventButton'
 
 const NewEventScreen = () => {
+  const [latitude, setLatitude] = useState()
+  const [longitude, setLongitude] = useState()
+
+  const selectedCoordinates = useSelector(selectedCoordinatesSelector)
+  const currentPosition = useSelector(currentPositionSelector)
+
+  useEffect(() => {
+    if (selectedCoordinates) {
+      setLatitude(selectedCoordinates.latitude)
+      setLongitude(selectedCoordinates.longitude)
+    } else {
+      if (currentPosition) {
+        setLatitude(currentPosition.latitude)
+        setLongitude(currentPosition.longitude)
+      }
+    }
+  }, [currentPosition, selectedCoordinates])
 
   return (
     <View>
-      <NewEventButton />
+      <NewEventButton latitude={latitude} longitude={longitude} />
     </View>
   )
 }
