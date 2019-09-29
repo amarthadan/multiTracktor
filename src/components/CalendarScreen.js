dfa4989c60fe1c319a975bd7d71e643e392222dc
Michal Kimle <kimle.michal@gmail.com>
2019-09-29 11:03:12 +0200

    Implement Calendar view

--- src/components/CalendarScreen.js
+++ src/components/CalendarScreen.js
@@ -1,13 +1,48 @@
-import React from 'react'
-
+import React, {Fragment, useState, useEffect} from 'react'
+import {NavigationEvents} from 'react-navigation'
 import {CalendarList} from 'react-native-calendars'
+import {format, parseISO} from 'date-fns'
+import {useNavigation} from 'react-navigation-hooks'
+
+import {EVENTS} from '../navigation/routes'
+import {getEvents, getEventByDate} from '../helpers/database'
 
 const CalendarScreen = () => {
+  const {navigate} = useNavigation()
+  const [events, setEvents] = useState([])
+  const [markedDates, setMarkedDates] = useState({})
+  const loadEvents = async () => setEvents(await getEvents())
+
+  const openEvent = async (dayData) => {
+    if (!Object.keys(markedDates).includes(dayData.dateString)) {
+      return
+    }
+
+    const event = await getEventByDate(parseISO(dayData.dateString))
+    navigate(EVENTS.EVENT, {eventId: event.id})
+  }
+
+  useEffect(() => {
+    setMarkedDates(events.reduce((object, event) => ({
+      ...object,
+      [format(event.timestamp, 'yyyy-MM-dd')]: {selected: true},
+    }), {}))
+  }, [events])
+
   return (
-    <CalendarList
-      pastScrollRange={3}
-      futureScrollRange={0}
-    />
+    <Fragment>
+      <NavigationEvents
+        onWillFocus={loadEvents}
+      />
+      <CalendarList
+        maxDate={new Date()}
+        firstDay={1}
+        onDayPress={openEvent}
+        futureScrollRange={0}
+        markedDates={markedDates}
+      />
+    </Fragment>
+
   )
 }
 
--- src/helpers/formatting.js
+++ src/helpers/formatting.js
@@ -1,5 +1,5 @@
-import {format, toDate} from 'date-fns'
+import {format} from 'date-fns'
 
 import {DATETIME_FORMAT} from '../constants'
 
-export const formatTimestamp = (timestamp) => format(toDate(timestamp), DATETIME_FORMAT)
+export const formatTimestamp = (timestamp) => format(timestamp, DATETIME_FORMAT)
